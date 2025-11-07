487f0bdae757f852bbc21c5267e520c43f708a1c
d4x3d <oladapodavid22354@gmail.com>
2025-11-07 01:04:35 +0100

    dgd

---
+++ convex/auth.config.ts
@@ -0,0 +1,7 @@
+// This file is for @convex-dev/auth configuration
+// For GitHub OAuth, we don't need additional config here
+// as it's configured in auth.ts with the GitHub provider
+
+export default {
+  // No additional providers needed - using GitHub OAuth from auth.ts
+};
